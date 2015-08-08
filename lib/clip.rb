85b69a3387e8e23a58eb405c3d9f730c358a9495
Hobogrammer <andrepsmithjr@gmail.com>
2015-08-08 13:47:52 -0700

    Fix garbage, correct clipboard format, change variable name

--- lib/clip.rb
+++ lib/clip.rb
@@ -1,22 +1,22 @@
 require 'win32/clipboard'
 include Win32
 
-def create_html(text)
+def create_html(current_text)
   vndump = File.new('vndump.html','w+')
-  file_contents(text, vndump)
+  file_contents(current_text, vndump)
 end
 
-def delete_file
+def delete_file #to be called on SIGINT
   File.delete('vndump.html') if File.exist?('vndump.html')
 end
 
-def file_contents(text, vndump)
+def file_contents(current_text, vndump)
   vndump.puts "<html>"
   vndump.puts "  <head>"
   vndump.puts "    <title>VN clipboard dump</title>"
   vndump.puts "  </head>"
   vndump.puts "  <body bgcolor='black'>"
-  vndump.puts "    <span><font color='white' size='18px'>#{text}</font></span>"
+  vndump.puts "    <span><font color='white' size='18px'>#{current_text}</font></span>"
   vndump.puts "  </body>"
   vndump.puts "</html>"
   vndump.close
@@ -31,10 +31,14 @@ def main()
   flag = true
   prev_text = ''
   puts "Now monitoring the clipboard for changes. Press Ctrl+C to close the program."
+  
   while flag #placeholder until I get some signal handling in
-    current_clip_text = Clipboard.data(CLIPBOARD::UNICODETEXT)
-    create_html(current_clip_text) if prev_text != current_clip_text
-    prev_text = current_clip_text
+    current_text = Clipboard.data(Clipboard::UNICODETEXT)
+    current_text = current_text.split("\u{000}")[0]
+    if prev_text != current_text
+      create_html(current_text)
+      prev_text = current_text
+    end
     sleep 0.5 
   end 
 end
