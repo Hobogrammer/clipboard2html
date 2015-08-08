086a26ef224d72030ce7acb26fa38cbc71cea4b0
Hobogrammer <andrepsmithjr@gmail.com>
2015-08-08 11:13:35 -0700

    Better implementation

--- lib/clip.rb
+++ lib/clip.rb
@@ -1,10 +1,5 @@
 require 'clipboard'
 
-def copy_clipboard_contents
-  text = Clipboard.paste
-  return text
-end
-
 def create_html(text)
   vndump = File.new('vndump.html','w+')
   file_contents(text, vndump)
@@ -20,24 +15,27 @@ def file_contents(text, vndump)
   vndump.puts "    <title>VN clipboard dump</title>"
   vndump.puts "  </head>"
   vndump.puts "  <body bgcolor='black'>"
-  vndump.puts "    <span><font color='white' size='24px'>#{text}</font></span>"
+  vndump.puts "    <span><font color='white' size='18px'>#{text}</font></span>"
   vndump.puts "  </body>"
   vndump.puts "</html>"
   vndump.close
 end
 
-def main()
-
-  flag = true
-
-  while flag
-    
-  current_clip_text = copy_clipboard_contents
-  create_html(current_clip_text) if text != current_clip_text
-    
-  sleep 0.5
-  
-  at_exit { delete_file } #This doesn't work. Signal handler needed
+def process_clip_change(current_text)
+  create_html(current_text)
 end
 
-run_loop
+
+def main()
+  flag = true
+  prev_text = ''
+  puts "Now monitoring the clipboard for changes. Press Ctrl+C to close the program."
+  while flag #placeholder until I get some signal handling in
+    current_clip_text = Clipboard.paste
+    create_html(current_clip_text) if prev_text != current_clip_text
+    prev_text = current_clip_text
+    sleep 0.5 
+  end 
+end
+
+main
